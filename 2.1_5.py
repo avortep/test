f94a4af1866454908fdc15b39a8d27e1aafa80d8
avortep <pudgessss@yandex.ru>
2021-02-05 10:31:22 +0400

    new

---
+++ 2.1_5.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # считываем значение для переменной x:
+    # найдём нужный элемент
+    x_element = browser.find_element_by_css_selector("#input_value")
+    # атрибут text возвращает текст, который находится между открывающим и закрывающим тегами элемента; скобки () здесь не нужны
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_css_selector("#answer")
+    input1.send_keys(y)
+
+    option1 = browser.find_element_by_css_selector("#robotCheckbox")
+    option1.click()
+
+    option2 = browser.find_element_by_css_selector("#robotsRule")
+    option2.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
